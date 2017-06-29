17df4358334892619872bb67a5ea2bdeb198dec3
Christopher Fujino <christopherfujino@gmail.com>
2017-06-29 16:47:34 -0700

    added: now shows running time

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,22 +1,33 @@
 console.log("Sanity Check: JS is working!");
 
+let startTime = 0;
+let endTime = 0;
+let running = false;
+
+let updateClock = function (lapsedTime) {
+  let seconds = Math.round(lapsedTime/100)/10;
+  $('#total-time').text(`${seconds} seconds`);
+}
+
+let timer = function () {
+  if(running) {
+    updateClock(Date.now() - startTime);
+  }
+}
+
 $(document).ready(function(){
-  let startTime = 0;
-  let endTime = 0;
-  let running = false;
   $(window).on('keydown', function (e) {
     if (e.which === 32) {
       if (running) {
         console.log('stopped!');
-        endTime = e.timeStamp;
-        let lapsedTime = Math.round((endTime - startTime) / 100 )/10;
-        $('#total-time').text(`${lapsedTime} seconds`);
+        endTime = Date.now();
+        updateClock(endTime - startTime);
         running = false;
       } else {
-        startTime = e.timeStamp;
-        $('#total-time').text('');
+        startTime = Date.now();
+        updateClock(0);
         running = true;
-        //window.setTimeout(function () {}, 100)
+        window.setInterval(timer, 100)
       }
     }
   });

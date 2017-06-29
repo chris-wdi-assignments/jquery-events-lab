067a7a13fd8eeb06383ec1c28a26720e091dd2e6
Christopher Fujino <christopherfujino@gmail.com>
2017-06-29 16:27:47 -0700

    added: stopwatch functionality

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,23 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+  let startTime = 0;
+  let endTime = 0;
+  let running = false;
+  $(window).on('keydown', function (e) {
+    if (e.which === 32) {
+      if (running) {
+        console.log('stopped!');
+        endTime = e.timeStamp;
+        let lapsedTime = Math.round((endTime - startTime) / 100 )/10;
+        $('#total-time').text(`${lapsedTime} seconds`);
+        running = false;
+      } else {
+        startTime = e.timeStamp;
+        $('#total-time').text('');
+        running = true;
+        //window.setTimeout(function () {}, 100)
+      }
+    }
+  });
 })

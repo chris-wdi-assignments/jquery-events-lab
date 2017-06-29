3146adf4abf0440fb869610e29c82f697b15344c
Christopher Fujino <christopherfujino@gmail.com>
2017-06-29 15:50:58 -0700

    added: on button click, a list element is appended describing event

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,10 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+  $('#imperatives').children().on('click', function (e) {
+    let buttonText = $(this).text();
+    let timeStamp = e.timeStamp;
+    let $li = $('<li>').text(`"${buttonText}" at ${timeStamp}`);
+    $('ul').append($li);
+  })
 })

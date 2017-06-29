80909279f24c6e736de639f4e72b4355e5a6df15
Christopher Fujino <christopherfujino@gmail.com>
2017-06-29 15:32:10 -0700

    added: implemented addition code

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  $('.add').on('change', function (e) {
+    let summand1 = Number($('#left').val());
+    let summand2 = Number($('#right').val());
+    $('#total').val(summand1 + summand2);
+  });
 })
